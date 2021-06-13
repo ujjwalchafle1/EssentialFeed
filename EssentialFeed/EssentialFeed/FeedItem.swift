ff90f4c5b0f6a172dbd02b4db2b7f2ce6d7516c9
Ujjwal Chafle <ujjwalchafle1@gmail.com>
2021-06-13 12:23:28 +0100

    Create FeedItem model

---
+++ EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Ujjwal on 13/06/2021.
+//
+
+import Foundation
+
+struct FeedItem {
+  let id: UUID
+  let description: String?
+  let location: String?
+  let imageURL: URL
+}
